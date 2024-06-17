ce96351175b3d8317e097585176ad539997c6f6e
Jeong-Dong-gi <159031134+Jeong-Dong-gi@users.noreply.github.com>
2024-06-17 16:45:51 +0900

    Service

---
+++ TodoService.java
@@ -0,0 +1,28 @@
+package com.todo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+	@Autowired
+	TodoRepo tRepo;
+	
+	// DB에 있는 전체 데이터를 가져 오기
+	public List<TodoEntity> getTodos() {
+		return tRepo.findAll();
+	}
+	
+	// 사용자에게 입력 받은 데이터를 DB에 저장
+	public void putTodo(TodoEntity tEntity) {
+		tRepo.save(tEntity);
+	}
+	
+	// 받은 id 값으로 삭제 하기
+	public void deleteTodo(Integer id) {
+		tRepo.deleteById(id);
+	}
+}
